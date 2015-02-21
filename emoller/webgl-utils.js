1f5457c647567e703be4de838a8a8afa73f72d80
hp <devprashantdawar@gmail.com>
2015-02-21 10:40:24 +0530

    emoller-webgl-06-xhr-shaders

--- emoller/webgl-utils.js
+++ emoller/webgl-utils.js
@@ -43,3 +43,56 @@ function screenQuad(){
 	*/  
 	return vertexPosBuffer;
 }
+
+function linkProgram(program){
+	var vshader = createShader(program.vshaderSource, gl.VERTEX_SHADER);
+	var fshader = createShader(program.fshaderSource, gl.FRAGMENT_SHADER);
+	gl.attachShader(program, vshader);
+	gl.attachShader(program, fshader);
+	gl.linkProgram(program);
+	if (!gl.getProgramParameter(program, gl.LINK_STATUS)){
+		throw gl.getProgramInfoLog(program);
+	}
+}
+
+function loadFile(file, callback, noCache){
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function(){
+		if (request.readyState == 1){
+			request.send();
+		} else if (request.readyState == 4){
+			if (request.status == 200){
+				callback(request.responseText);
+			} else if (request.status == 404){
+				throw 'File"' + file +'" does not exist.';
+			} else {
+				throw ' XHR error ' + request.status + '.';
+			}
+		}
+	};
+	var url = file;
+	if (noCache){
+		url += '?' + (new Date()).getTime();
+	}
+	request.open('GET', file, true);
+}
+function loadProgram(vs, fs, callback){
+	var program = gl.createProgram;
+	function vshaderLoaded(str){
+		program.vshaderSource = str;
+		if (program.fshaderSource){
+			linkProgram(program);
+			callback(program);
+		}
+	}
+
+	function fshaderLoaded(str){
+		program.fshaderSource = str;
+		if (program.vshaderSource){
+			linkProgram(program);
+			callback(program);
+		}
+	}
+	loadFile(vs, vshaderLoaded, true);
+	loadFile(fs, fshaderLoaded, true);
+}
